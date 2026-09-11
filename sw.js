f065d5b96c343b2945457b8eeba990994a7c6935
basakekmekkepsut <basakekmekkepsut@gmail.com>
2026-09-11 19:19:20 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Uygulama dosyaları önbelleğe alınır (çevrimdışı açılsın diye),
 // ama Firebase/Telegram istekleri ASLA önbellekten verilmez - veri hep canlıdır.
 
-const CACHE = 'okul-ekmek-v3-2';
+const CACHE = 'okul-ekmek-v3-3';
 const SHELL = [
   './',
   './index.html',
